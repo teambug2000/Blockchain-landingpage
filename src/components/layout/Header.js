08d832a55810107acfab01e118b735c624b7b1ba
Loc Duong <teambug2000@gmail.com>
2022-12-06 13:30:03 +0700

    language

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,25 +1,25 @@
-import React, { useState, useRef, useEffect } from 'react';
-import PropTypes from 'prop-types';
-import classNames from 'classnames';
-import { Link } from 'react-router-dom';
-import Logo from './partials/Logo';
-import Image from '../elements/Image';
+import React, { useState, useRef, useEffect } from "react";
+import PropTypes from "prop-types";
+import classNames from "classnames";
+import { Link } from "react-router-dom";
+import Logo from "./partials/Logo";
+import Image from "../elements/Image";
 
 const propTypes = {
   navPosition: PropTypes.string,
   hideNav: PropTypes.bool,
   hideSignin: PropTypes.bool,
   bottomOuterDivider: PropTypes.bool,
-  bottomDivider: PropTypes.bool
-}
+  bottomDivider: PropTypes.bool,
+};
 
 const defaultProps = {
-  navPosition: '',
+  navPosition: "",
   hideNav: false,
   hideSignin: false,
   bottomOuterDivider: false,
-  bottomDivider: false
-}
+  bottomDivider: false,
+};
 
 const Header = ({
   className,
@@ -30,7 +30,6 @@ const Header = ({
   bottomDivider,
   ...props
 }) => {
-
   const [isActive, setIsactive] = useState(false);
 
   const nav = useRef(null);
@@ -38,64 +37,68 @@ const Header = ({
 
   useEffect(() => {
     isActive && openMenu();
-    document.addEventListener('keydown', keyPress);
-    document.addEventListener('click', clickOutside);
+    document.addEventListener("keydown", keyPress);
+    document.addEventListener("click", clickOutside);
     return () => {
-      document.removeEventListener('keydown', keyPress);
-      document.removeEventListener('click', clickOutside);
+      document.removeEventListener("keydown", keyPress);
+      document.removeEventListener("click", clickOutside);
       closeMenu();
     };
-  });  
+  });
 
   const openMenu = () => {
-    document.body.classList.add('off-nav-is-active');
-    nav.current.style.maxHeight = nav.current.scrollHeight + 'px';
+    document.body.classList.add("off-nav-is-active");
+    nav.current.style.maxHeight = nav.current.scrollHeight + "px";
     setIsactive(true);
-  }
+  };
 
   const closeMenu = () => {
-    document.body.classList.remove('off-nav-is-active');
+    document.body.classList.remove("off-nav-is-active");
     nav.current && (nav.current.style.maxHeight = null);
     setIsactive(false);
-  }
+  };
 
   const keyPress = (e) => {
     isActive && e.keyCode === 27 && closeMenu();
-  }
+  };
 
   const clickOutside = (e) => {
-    if (!nav.current) return
-    if (!isActive || nav.current.contains(e.target) || e.target === hamburger.current) return;
+    if (!nav.current) return;
+    if (
+      !isActive ||
+      nav.current.contains(e.target) ||
+      e.target === hamburger.current
+    )
+      return;
     closeMenu();
-  }  
+  };
 
   const classes = classNames(
-    'site-header',
-    bottomOuterDivider && 'has-bottom-divider',
+    "site-header",
+    bottomOuterDivider && "has-bottom-divider",
     className
   );
 
   return (
-    <header
-      {...props}
-      className={classes}
-    >
+    <header {...props} className={classes}>
       <div className="container">
-        <div className={
-          classNames(
-            'site-header-inner',
-            bottomDivider && 'has-bottom-divider'
-          )}>
-          {!hideNav &&
-          <Image
-            className="header-nav-toggle"
-            src={require('./../../assets/images/avatar2.svg')}
-            alt="avatar2"
-            width={32}
-            height={32} 
-          />}
+        <div
+          className={classNames(
+            "site-header-inner",
+            bottomDivider && "has-bottom-divider"
+          )}
+        >
+          {!hideNav && (
+            <Image
+              className="header-nav-toggle"
+              src={require("./../../assets/images/avatar2.svg")}
+              alt="avatar2"
+              width={32}
+              height={32}
+            />
+          )}
           <Logo />
-          {!hideNav &&
+          {!hideNav && (
             <>
               <button
                 ref={hamburger}
@@ -109,50 +112,60 @@ const Header = ({
               </button>
               <nav
                 ref={nav}
-                className={
-                  classNames(
-                    'header-nav',
-                    isActive && 'is-active'
-                  )}>
+                className={classNames("header-nav", isActive && "is-active")}
+              >
                 <div className="header-nav-inner">
-                  {!hideSignin &&
-                    <ul
-                      className="list-reset header-nav-right"
-                    >
+                  {!hideSignin && (
+                    <ul className="list-reset header-nav-right">
                       <li>
-                        <Link to="#0" onClick={closeMenu}>About us</Link>
+                        <Link to="#0" onClick={closeMenu}>
+                          About us
+                        </Link>
                       </li>
                       <li>
-                        <Link to="#0" onClick={closeMenu}>Services</Link>
+                        <Link to="#0" onClick={closeMenu}>
+                          Services
+                        </Link>
                       </li>
                       <li>
-                        <Link to="#0" onClick={closeMenu}>How it works</Link>
+                        <Link to="#0" onClick={closeMenu}>
+                          How it works
+                        </Link>
                       </li>
                       <li>
-                        <Link to="#0" onClick={closeMenu}>Community</Link>
+                        <Link to="#0" onClick={closeMenu}>
+                          Community
+                        </Link>
                       </li>
                       <li>
-                        <select>
-                          <option>VI</option>
-                          <option>EN</option>
-                          <option>DE</option>
-                          <option>JA</option>
-                          <option>KR</option>
+                        <select className="nav-link bg-dark border-0">
+                          <option value="vi">VI</option>
+                          <option value="en">EN</option>
+                          <option value="de">DE</option>
+                          <option value="ja">JA</option>
+                          <option value="kr">KR</option>
                         </select>
                       </li>
                       <li>
-                        <Link to="#0" className="button button-primary button-wide-mobile button-sm btn-signup" onClick={closeMenu}>Join for Free</Link>
+                        <Link
+                          to="#0"
+                          className="button button-primary button-wide-mobile button-sm btn-signup"
+                          onClick={closeMenu}
+                        >
+                          Join for Free
+                        </Link>
                       </li>
-                    </ul>}
+                    </ul>
+                  )}
                 </div>
               </nav>
-              
-            </>}
+            </>
+          )}
         </div>
       </div>
     </header>
   );
-}
+};
 
 Header.propTypes = propTypes;
 Header.defaultProps = defaultProps;
